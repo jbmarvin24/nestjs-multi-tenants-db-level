e9de26c00f972350feb31095862242a8108c6f39
John Marvin Doniña <jbmarvin24@gmail.com>
2024-06-12 15:26:42 +0800

    Removed deprecated functions

--- src/modules/tenancy/tenancy.utils.ts
+++ src/modules/tenancy/tenancy.utils.ts
@@ -1,40 +1,27 @@
-import {
-  Connection,
-  DataSource,
-  createConnection,
-  getConnectionManager,
-} from 'typeorm';
+import { DataSource } from 'typeorm';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import { tenantOrmConfig } from '../../tenants-orm.config';
 
-// export function getTenantConnection(tenantId: string): Promise<Connection> {
-//   const connectionName = `tenant_${tenantId}`;
-//   const connectionManager = getConnectionManager();
+const dataSources: { [key: string]: DataSource } = {};
 
-//   if (connectionManager.has(connectionName)) {
-//     const connection = connectionManager.get(connectionName);
-//     return Promise.resolve(
-//       connection.isConnected ? connection : connection.connect(),
-//     );
-//   }
-
-//   return createConnection({
-//     ...(tenantOrmConfig as MysqlConnectionOptions),
-//     name: connectionName,
-//     database: connectionName,
-//   });
-// }
-
-export function getTenantConnection(tenantId: string) {
+export async function getTenantConnection(tenantId: string) {
   const databaseName = `tenant_${tenantId}`;
-  const dataSource = new DataSource({
+
+  if (dataSources[databaseName]) {
+    const dataSource = dataSources[databaseName];
+    if (!dataSource.isInitialized) {
+      await dataSource.initialize();
+    }
+
+    return dataSource;
+  }
+
+  const newDataSource = new DataSource({
     ...(tenantOrmConfig as MysqlConnectionOptions),
     database: databaseName,
   });
 
-  console.log('dataSource.isInitialized:', dataSource.isInitialized);
-
-  return Promise.resolve(
-    dataSource.isInitialized ? dataSource : dataSource.initialize(),
-  );
+  dataSources[databaseName] = newDataSource;
+  await newDataSource.initialize();
+  return newDataSource;
 }
